ce14e35fda95b1aaf8af1fc75cebf405d97c93d0
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-01 21:00:27 +0500

    Javob 3

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char a=97,c=99,h=104,r=11;
-        System.out.println(c);
-        System.out.println(h);
-        System.out.println(a);
-        System.out.println(r);
+        // int c,h,a,r;
+         char a=97,c=99,h=104,r=114;
+         System.out.println(c);
+         System.out.println(h);
+         System.out.println(a);
+         System.out.println(r);
+ 
+ 
     }
 }
