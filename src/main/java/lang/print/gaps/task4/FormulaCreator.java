fdc7232189753566802e03926f223cb05595e39e
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-01 21:11:08 +0500

    sinov

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,8 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
             int a=1;
             int b=2;
-            double c;
-            c=(9 * Math.pow(a, 2)-5 * b + 2 + a - 7)*(( a + b - 4 * a * b ) / 2);
-            System.out.println(c);
+            double res = (9 * Math.pow(a, 2)-5 * b + 2 + a - 7)*(( a + b - 4 * a * b ) / 2);
+            System.out.println(res);
         }
 }
